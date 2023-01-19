7693d0fc2c103b9f086f46df3d2503f21b6a4604
segfault <segfault@riseup.net>
2023-01-19 22:47:01 +0100

    WIP: Tor Browser: Allow access to files opened via portals
    Same setup as for Thunderbird (#19157).
    
    Closes #10422
    
    TODO:
    * Add test
    * Check if we can remove some of the bwrap bindings

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/netnsdrop.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/netnsdrop.py
@@ -10,6 +10,7 @@ a somewhat similar structure. This is:
 """
 import os
 import logging
+import sh
 
 from tailslib.gnome import gnome_env_vars
 from tailslib import LIVE_USERNAME
@@ -20,9 +21,38 @@ A11Y_BUS_SANDBOX_PATH="/run/user/1000/tails-sandbox/a11y-bus-proxy.sock"
 IBUS_SANDBOX_PATH="/run/user/1000/tails-sandbox/ibus-proxy.sock"
 
 
-def run_in_netns(*args, netns, user="amnesia", root="/", bind_mounts=[]):
+def run_in_netns(*args, netns, user="amnesia", root="/", bind_mounts=[], flatpak_info: str = None, app_id: str = None):
     # base bwrap sharing most of the system
-    bwrap = ["bwrap", "--bind", root, "/", "--proc", "/proc", "--dev", "/dev"]
+    bwrap = ["bwrap"]
+
+    if flatpak_info and root == "/":
+        bwrap += [
+             "--proc", "/proc",
+             "--dev", "/dev",
+             # TODO: We might be able to drop some of these bindings
+             "--bind", "/bin", "/bin",
+             "--bind", "/etc", "/etc",
+             "--bind", "/home", "/home",
+             "--bind", "/lib", "/lib",
+             "--bind", "/lib64", "/lib64",
+             "--bind", "/live", "/live",
+             "--bind", "/media", "/media",
+             "--bind", "/tmp", "/tmp",
+             "--bind", "/usr", "/usr",
+             "--bind", "/run", "/run",
+             "--bind", "/sbin", "/sbin",
+             "--bind", "/sys", "/sys",
+             "--bind", "/var", "/var",
+        ]
+    else:
+        bwrap += ["--bind", root, "/", "--proc", "/proc", "--dev", "/dev"]
+
+    if flatpak_info:
+        bwrap += ["--bind", flatpak_info, "/.flatpak-info"]
+
+    if app_id:
+        bwrap += ["--bind", f"/run/user/1000/doc/by-app/{app_id}", "/run/user/1000/doc"]
+
     for src, dest in bind_mounts:
         bwrap += ["--bind", src, dest]
 
@@ -57,11 +87,13 @@ def run(
     netns: str,
     wrapper_executable: str,
     keep_env=True,
+    app_id: str = None,
+    flatpak_info: str = None,
     extra_env={},
     extra_args=[],
 ):
     if os.getuid() == 0:
-        run_in_netns(real_executable, *extra_args, netns=netns)
+        run_in_netns(real_executable, *extra_args, netns=netns, app_id=app_id, flatpak_info=flatpak_info)
     else:
         env = os.environ.copy() if keep_env else {}
         env.update(extra_env)
