5cef96cba6e832f7f27ee991ddc656494dde39f2
intrigeri <intrigeri@boum.org>
2023-10-18 08:15:54 +0000

    Test suite: dont import isotesters' snakeoil SSL cert anymore
    It's not needed anymore: since tails/sysadmin#17990, the test suite
    will use 1 single test email server that has valid SSL certificates.
    
    refs #19193

--- features/step_definitions/thunderbird.rb
+++ features/step_definitions/thunderbird.rb
@@ -20,25 +20,6 @@ def thunderbird_inbox
   folder_view.child(/^Inbox|Inbox (.*)$/, roleName: 'table row', recursive: false)
 end
 
-def thunderbird_install_host_snakeoil_ssl_cert
-  # Inspiration:
-  # * https://wiki.mozilla.org/CA:AddRootToFirefox
-  # * https://mike.kaply.com/2015/02/10/installing-certificates-into-firefox/
-  debug_log('Installing host snakeoil SSL certificate')
-  local_cert_path = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
-  vm_db_path = "/home/#{LIVE_USER}/.thunderbird/profile.default/cert9.db"
-  local_db_path = './cert9.db'
-  local_db_uri = "sql:#{File.dirname(local_db_path)}"
-  File.unlink(local_db_path) if File.exist?(local_db_path)
-
-  cmd_helper(['certutil', '-d', local_db_uri, '-A', '-i', local_cert_path, '-n', 'snakeoil', '-t', 'TCu,TCu,TCu'])
-
-  $vm.execute_successfully("mkdir -p #{File.dirname(vm_db_path)}")
-  assert File.exist?(local_db_path)
-  $vm.file_copy_local(local_db_path, vm_db_path)
-  $vm.execute_successfully("chown -R #{LIVE_USER}. /home/#{LIVE_USER}/.thunderbird/")
-end
-
 When /^I start Thunderbird$/ do
   workaround_pref_lines = [
     # When we generate a random subject line it may contain one of the
@@ -49,9 +30,6 @@ When /^I start Thunderbird$/ do
   workaround_pref_lines.each do |line|
     $vm.file_append('/etc/thunderbird/pref/thunderbird.js', line + "\n")
   end
-  # On Jenkins each isotester runs its own email server, using their
-  # respective snakeoil SSL cert, so we have to import it.
-  thunderbird_install_host_snakeoil_ssl_cert unless ENV['JENKINS_URL'].nil?
   step 'I start "Thunderbird" via GNOME Activities Overview'
   try_for(60) { thunderbird_main }
 end
