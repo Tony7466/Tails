0a2192ae4df3dbebe57cac1ffc57f7c4713b0f05
Zen Fu <zen@systemli.org>
2023-11-14 11:05:24 -0300

    Improve script documentation
    refs sysadmin#19947

--- bin/recover-lost-translations.py
+++ bin/recover-lost-translations.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 """
+Recover translations that were lost between two commits.
+
+This script is meant to be used in situations where translations were
+mistakenly invalidated, for example because of unintended changes in source
+strings.
 """
 
 import git
@@ -13,7 +18,13 @@ import sys
 import pathlib
 
 
-def recover_from_old(e_old, e_new):
+def should_be_recovered(e_old, e_new):
+    """
+    Decide whether a translation should be recovered or not.
+
+    Return True if the translation is valid in the old entry and invalid (and
+    not obsolete) in the new entry.
+    """
     if not e_old.translated():
         return False
 
@@ -23,81 +34,86 @@ def recover_from_old(e_old, e_new):
     return True
 
 
-def copy(e_to, e_from):
+def copy(e_old, e_new):
     """
-    Copies the content from a given entry to a given entry.
+    Copies the content from an old entry to a new entry.
     """
-    e_to.msgid = e_from.msgid
-    e_to.msgstr = e_from.msgstr
-    e_to.occurrences = e_from.occurrences
-    e_to.comment = e_from.comment
-    e_to.flags = e_from.flags[:]  # clone flags
-    e_to.msgid_plural = e_from.msgid_plural
-    e_to.obsolete = e_from.obsolete
-    if e_from.msgstr_plural:
-        for pos in e_from.msgstr_plural:
+    e_new.msgid = e_old.msgid
+    e_new.msgstr = e_old.msgstr
+    e_new.occurrences = e_old.occurrences
+    e_new.comment = e_old.comment
+    e_new.flags = e_old.flags[:]  # clone flags
+    e_new.msgid_plural = e_old.msgid_plural
+    e_new.obsolete = e_old.obsolete
+    if e_old.msgstr_plural:
+        for pos in e_old.msgstr_plural:
             try:
                 # keep existing translation at pos if any
-                e_to.msgstr_plural[pos]
+                e_new.msgstr_plural[pos]
             except KeyError:
-                e_to.msgstr_plural[pos] = ''
+                e_new.msgstr_plural[pos] = ''
 
 
-
-logger = logging.getLogger(__name__)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--width', '-w', type=int, default=80, help='Width to wrap lines')
-parser.add_argument('old_ref', help='commit before the probleatic commit')
-parser.add_argument('new_ref', default='HEAD', help='commit onto you recover. normally you want  to use HEAD here.', nargs='?')
-args = parser.parse_args()
-
-repo = git.Repo("")
-old = repo.commit(args.old_ref)
-diff = old.diff(args.new_ref)
-
-logging.basicConfig()
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--width', '-w', type=int, default=80, help='Width to wrap lines')
+    parser.add_argument('old_ref', help='commit before the problematic commit')
+    parser.add_argument('new_ref', default='HEAD', help='commit onto you recover. normally you want  to use HEAD here.', nargs='?')
+    return parser.parse_args()
 
 
+def recover_lost_translations(args, logger):
+    """
+    Recover translations that were valid in an old commit and are invalid in a
+    new commit.
+    """
+    repo = git.Repo("")
+    old = repo.commit(args.old_ref)
+    diff = old.diff(args.new_ref)
 
+    git_base = pathlib.Path(repo.git_dir).parent
 
-changed = False
-
-git_base = pathlib.Path(repo.git_dir).parent
-
-for i in diff:
-    if not i.b_path.endswith(".po"):
-        continue
-    if i.change_type not in ("R", "M"):
-        continue
-
-    a = i
-    try:
-        pofile_old = polib.pofile(i.a_blob.data_stream.read().decode("utf-8"), encoding="utf-8", wrapwidth=args.width)
-    except OSError as e:
-        logger.warning(f"{i.a_path}@{args.old_ref}: {e}")
-        continue
-
-    try:
-        pofile_new = polib.pofile(i.b_blob.data_stream.read().decode("utf-8"), encoding="utf-8", wrapwidth=args.width)
-    except OSError as e:
-        logger.warning(f"{i.b_path}@{args.new_ref}: {e}")
-        continue
-
-    changed_file = False
-
-    for e_new in pofile_new:
-        e_old = pofile_old.find(e_new.msgid, by="msgid")
-        if not e_old:
+    for f in diff:
+        if not f.b_path.endswith(".po"):
+            continue
+        if f.change_type not in ("R", "M"):
             continue
 
-        if recover_from_old(e_old, e_new):
-            changed_file = True
-            copy(e_new, e_old)
+        try:
+            pofile_old = polib.pofile(f.a_blob.data_stream.read().decode("utf-8"), encoding="utf-8", wrapwidth=args.width)
+        except OSError as e:
+            logger.warning(f"{f.a_path}@{args.old_ref}: {e}")
+            continue
 
-    if changed_file:
-        newpath = i.b_path
-        subprocess.run(['msgcat', '--width', str(args.width), '-t', 'utf-8' ,'-o', str(git_base/newpath), '-'],
-                       input=pofile_new.__unicode__().encode('utf-8'),
-                       check=True)
-        changed = True
+        try:
+            pofile_new = polib.pofile(f.b_blob.data_stream.read().decode("utf-8"), encoding="utf-8", wrapwidth=args.width)
+        except OSError as e:
+            logger.warning(f"{f.b_path}@{args.new_ref}: {e}")
+            continue
+
+        changed_file = False
+
+        for e_new in pofile_new:
+            e_old = pofile_old.find(e_new.msgid, by="msgid")
+            if not e_old:
+                continue
+
+            if should_be_recovered(e_old, e_new):
+                changed_file = True
+                copy(e_old, e_new)
+
+        if changed_file:
+            newpath = f.b_path
+            subprocess.run(['msgcat', '--width', str(args.width), '-t', 'utf-8' ,'-o', str(git_base/newpath), '-'],
+                           input=pofile_new.__unicode__().encode('utf-8'),
+                           check=True)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    args = parse_args()
+    recover_lost_translations(args, logger)
