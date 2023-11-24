Tony7466/Tails

commit 5b46d28df4f40418cd6cde90b1fbbc9dd48b3c14

Manually correct or disable Rubocop offenses

features/support/extra_hooks.rb
@@ -67,11 +67,12 @@ unless $at_exit_print_artifacts_dir_patching_done
       pending:   'pending',
       skipped:   'skipped',
     }.freeze
+    private_constant :STATUS_STR
 
     # Support printing the step status. For the original function body,
     # see https://salsa.debian.org/ruby-team/cucumber/-/blob/9899bc47c0eac62b623208f5e8032ec7285fe257/lib/cucumber/formatter/console.rb#L33-44
     alias old_format_step format_step
-    def format_step(keyword, step_match, status, source_indent, print_status = false)
+    def format_step(keyword, step_match, status, source_indent, print_status: false)
       comment = if source_indent
                   c = "# #{step_match.location}".indent(source_indent)
                   format_string(c, :comment)
@@ -190,8 +191,8 @@ module ExtraFormatters
     end
 
     # Recursively print the exception and all previous exceptions
-    def print_exception(e, status, indent)
-      super(e, status, indent)
+    def print_exception(exc, status, indent)
+      super(exc, status, indent)
       if e.cause
         cause = Cucumber::Formatter::BacktraceFilter.new(e.cause.dup).exception
         print_exception(cause, status, indent)

.rubocop.yml
@@ -64,6 +64,12 @@ Naming/MethodName:
     - \A(doubleClick|roleName)\z
     # Stay close to OpenCV's API
     - \AmatchTemplate\z
+    # Stay close to Dogtail's API
+    - \AchildLabelled\z
+    - \AchildNamed\z
+    - \AmenuItem\z
+    - \AdoActionNamed\z
+    - \AgrabFocus\z
 
 Naming/MethodParameterName:
   AllowedNames:

features/step_definitions/additional_software_packages.rb
@@ -87,10 +87,9 @@ Then /^"([^"]*)" is not in the list of Additional Software$/ do |package|
   end
 end
 
-When /^I (refuse|accept) (adding|removing) "([^"]*)" (?:to|from) Additional Software$/ do |decision, action, package|
+When /^I (refuse|accept) (adding|removing) "(?:[^"]*)" (?:to|from) Additional Software$/ do |decision, action|
   case action
   when 'adding'
-    notification_title = "Add #{package} to your additional software?"
     case decision
     when 'accept'
       button_title = 'Install Every Time'
@@ -98,7 +97,6 @@ When /^I (refuse|accept) (adding|removing) "([^"]*)" (?:to|from) Additional Soft
       button_title = 'Install Only Once'
     end
   when 'removing'
-    notification_title = "Remove #{package} from your additional software?"
     case decision
     when 'accept'
       button_title = 'Remove'

features/step_definitions/apt.rb
@@ -23,7 +23,8 @@ end
 Then /^no experimental APT suite is enabled for deb[.]torproject[.]org$/ do
   # apow7mjfryruh65chtdydfmqfpj5btws7nbocgtaovhvezgccyjazpqd.onion == deb.torproject.org
   assert_no_match(
-    /apow7mjfryruh65chtdydfmqfpj5btws7nbocgtaovhvezgccyjazpqd[.]onion.*experimental/, apt_sources
+    /apow7mjfryruh65chtdydfmqfpj5btws7nbocgtaovhvezgccyjazpqd[.]onion.*experimental/,
+    apt_sources
   )
 end
 
@@ -32,8 +33,11 @@ Then /^if releasing, the tagged Tails APT source is enabled$/ do
     puts 'Not on a tag ⇒ skipping this step'
     next
   end
-  assert_match(%r{umjqavufhoix3smyq6az2sx4istmuvsgmz4bq5u5x56rnayejoo6l2qd[.]onion/?\s+#{Regexp.quote(git_current_tag)}\s},
-               apt_sources)
+  custom_onion = 'umjqavufhoix3smyq6az2sx4istmuvsgmz4bq5u5x56rnayejoo6l2qd.onion'
+  assert_match(
+    %r{#{Regexp.quote(custom_onion)}/?\s+#{Regexp.quote(git_current_tag)}\s},
+    apt_sources
+  )
 end
 
 Then /^if releasing, no unversioned Tails APT source is enabled$/ do
@@ -41,8 +45,11 @@ Then /^if releasing, no unversioned Tails APT source is enabled$/ do
     puts 'Not on a tag ⇒ skipping this step'
     next
   end
-  assert_no_match(%r{umjqavufhoix3smyq6az2sx4istmuvsgmz4bq5u5x56rnayejoo6l2qd[.]onion/?\s+(stable|testing|devel)\s},
-                  apt_sources)
+  custom_onion = 'umjqavufhoix3smyq6az2sx4istmuvsgmz4bq5u5x56rnayejoo6l2qd.onion'
+  assert_no_match(
+    %r{#{Regexp.quote(custom_onion)}/?\s+(stable|testing|devel)\s},
+    apt_sources
+  )
 end
 
 When /^I configure APT to use non-onion sources$/ do

features/step_definitions/browser.rb
@@ -97,8 +97,10 @@ When(/^I kill the ((?:Tor|Unsafe) Browser)$/) do |browser|
     $vm.execute("pgrep --full --exact '#{info[:cmd_regex]}'").failure?
   end
 
-  # ugly fix to #18568; in my local testing, 3 seconds are always needed. Let's add some more.
-  # a better solution would be to wait until GNOME "received" the fact that Tor Browser has gone away.
+  # Ugly fix to #18568; in my local testing, 3 seconds are always needed.
+  # Let's add some more.
+  # A better solution would be to wait until GNOME "received" the fact
+  # that the browser has gone away.
   sleep 5
 end
 

features/step_definitions/build.rb
@@ -40,7 +40,8 @@ Given %r{^the last version mentioned in debian/changelog is ([[:alnum:]~.]+)$} d
   raise StandardError, 'dpkg-parsechangelog failed.' if $CHILD_STATUS != 0
 
   if last != version
-    fatal_system "DEBEMAIL=tails@boum.org debchange -v '#{version}' 'New upstream release'"
+    fatal_system 'DEBEMAIL=tails@boum.org ' \
+                 "debchange -v '#{version}' 'New upstream release'"
   end
 end
 

features/step_definitions/checks.rb
@@ -230,8 +230,9 @@ Then /^Tor is (not )?confined with Seccomp$/ do |not_confined|
   sandbox_status = $vm.execute_successfully(
     '/usr/local/lib/tor_variable get --type=conf Sandbox'
   ).stdout.to_i
+  sandbox_status_str = sandbox_status == 1 ? 'enabled' : 'disabled'
   assert_equal(expected_sandbox_status, sandbox_status,
-               "Tor says that the sandbox is #{sandbox_status == 1 ? 'enabled' : 'disabled'}")
+               "Tor says that the sandbox is #{sandbox_status_str}")
   # tor's Seccomp status will always be 2 (filter mode), even with
   # "Sandbox 0", but let's still make sure that is the case.
   seccomp_status = get_seccomp_status('tor')

features/step_definitions/common_steps.rb
@@ -234,7 +234,8 @@ Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I 
   step 'the computer boots Tails'
   if do_login
     step 'I enable persistence' if persistence_on
-    step 'I enable persistence with the changed passphrase' if persistence_with_changed_passphrase
+    step 'I enable persistence with the changed passphrase' \
+      if persistence_with_changed_passphrase
     step 'I set an administration password' if admin_password
     step 'I log in to a new session'
     step 'the Additional Software installation service has started'
@@ -532,7 +533,8 @@ Given /^the Tails desktop is ready$/ do
   )
   # Optimize upgrade check: avoid 30 second sleep
   $vm.execute_successfully(
-    'sed -i "s/^ExecStart=.*$/& --no-wait/" /usr/lib/systemd/user/tails-upgrade-frontend.service'
+    'sed -i "s/^ExecStart=.*$/& --no-wait/" ' \
+    '/usr/lib/systemd/user/tails-upgrade-frontend.service'
   )
   $vm.execute_successfully('systemctl --user daemon-reload', user: LIVE_USER)
 end
@@ -549,7 +551,8 @@ When /^I see the "(.+)" notification(?: after at most (\d+) seconds)?$/ do |titl
 end
 
 Given /^Tor is ready$/ do
-  # deprecated: please choose between "I successfully configure Tor" and "I wait until Tor is ready"
+  # deprecated: please choose between "I successfully configure Tor"
+  # and "I wait until Tor is ready"
   step 'I successfully configure Tor'
 end
 
@@ -875,8 +878,8 @@ When /^I copy "([^"]+)" to "([^"]+)" as user "([^"]+)"$/ do |source, destination
   assert(c.success?, "Failed to copy file:\n#{c.stdout}\n#{c.stderr}")
 end
 
-def is_persistence_active?(app)
-  conf = get_tps_bindings(true)[app.to_s]
+def persistence_active?(app)
+  conf = get_tps_bindings(skip_links: true)[app.to_s]
   c = $vm.execute("findmnt --noheadings --output SOURCE --target '#{conf}'")
   c.success? && (c.stdout.chomp != 'overlay')
 end
@@ -884,9 +887,9 @@ end
 Then /^persistence for "([^"]+)" is (|not )active$/ do |app, active|
   case active
   when ''
-    assert(is_persistence_active?(app), 'Persistence should be active.')
+    assert(persistence_active?(app), 'Persistence should be active.')
   when 'not '
-    assert(!is_persistence_active?(app), 'Persistence should not be active.')
+    assert(!persistence_active?(app), 'Persistence should not be active.')
   end
 end
 

commit 8ce16f845d1a15902c78fa83183196b2db507413

Fix regression introduced by partial renaming in 5b46d28df4f40418cd6cde90b1fbbc9dd48b3c14

features/support/extra_hooks.rb
@@ -193,8 +193,8 @@ module ExtraFormatters
     # Recursively print the exception and all previous exceptions
     def print_exception(exc, status, indent)
       super(exc, status, indent)
-      if e.cause
-        cause = Cucumber::Formatter::BacktraceFilter.new(e.cause.dup).exception
+      if exc.cause
+        cause = Cucumber::Formatter::BacktraceFilter.new(exc.cause.dup).exception
         print_exception(cause, status, indent)
       end
     end