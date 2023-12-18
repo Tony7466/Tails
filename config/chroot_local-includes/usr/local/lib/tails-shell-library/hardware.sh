862818e193841c4c67cd557eb6b0cf624331a627
boyska <boyska@riseup.net>
2023-12-18 17:45:20 +0100

    catch disk errors in flag file

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/hardware.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/hardware.sh
@@ -105,3 +105,17 @@ unload_module_and_rev_deps() {
   # shellcheck disable=SC2046
   /sbin/modprobe -r $(mod_rev_dep "${1}")
 }
+
+is_first_boot() {
+    # This code is duplicating config/chroot_local-includes/usr/share/initramfs-tools/scripts/init-premount/partitioning
+    # please keep this in sync!
+    # XXX: bookworm (usrmerge): this might have become /usr/sbin/sgdisk
+    
+    if test -L /dev/bilibop; then
+        GUID="$(/sbin/sgdisk --print /dev/bilibop \
+                | sed -n '/^Disk identifier (GUID)/ s/^Disk identifier (GUID): // p')"
+        test "${GUID}" = "17B81DA0-8B1E-4269-9C39-FE5C7B9B58A3"
+    else
+        false
+    fi
+}
