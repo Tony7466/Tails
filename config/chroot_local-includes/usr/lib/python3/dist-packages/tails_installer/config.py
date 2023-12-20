9e6be42113f0718fc763055cfaf7bbe7f4e6e6be
Ben Westgate <benwestgate@protonmail.com>
2023-12-20 01:04:08 +0000

    Add `luks2_header_size` and `persistence_mountpoint` keys same as !1318 and !1303
    Used by get_persistent_storage_size() function from tails_installer/utils.py

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tails_installer/config.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tails_installer/config.py
@@ -4,7 +4,7 @@ CONFIG = {
     # Minimum device size we accept as valid target for initial
     # installation, in MiB as in 1 MiB = 1024**2 bytes. I've seen USB
     # sticks labeled "8 GB" that were 7759462400 bytes = 7400 MiB
-    # large, and one can probably fine even smaller ones, so let's be
+    # large, and one can probably find even smaller ones, so let's be
     # nice with users who believed what was written on the box and
     # accept slightly smaller devices than what the theory
     # would dictate.
@@ -13,8 +13,11 @@ CONFIG = {
     # as in 1000 MB = 1 GB, i.e. let's use a unit close to what they will
     # see displayed in shops.
     "official_min_installation_device_size": 8000,
+    # Space used in MiB for the luks2 header.
+    "luks2_header_size": 16,
     "main_liveos_dir": "live",
     "running_liveos_mountpoint": "/lib/live/mount/medium",
+    "persistence_mountpoint": "/live/persistence/TailsData_unlocked",
     "liveos_toplevel_files": [
         "autorun.bat",
         "autorun.inf",
