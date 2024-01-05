e8c3d378c16f89b22a81387b6c2f7810fea6d2c2
Sandro Knauß <hefee@debian.org>
2024-01-05 12:20:56 +0100

    fix typos and make rubocop happy.

--- features/step_definitions/hardware_failure.rb
+++ features/step_definitions/hardware_failure.rb
@@ -1,9 +1,9 @@
 When /^Tails detect disk read failures$/ do
-  squashfs_failed = "/var/lib/live/tails.squashfs_failed"
-  $m.execute("touch #{squashfs_failed}")
+  squashfs_failed = '/var/lib/live/tails.squashfs_failed'
+  $vm.execute("touch #{squashfs_failed}")
   try_for(60) { $vm.file_exist?(squashfs_failed) }
 end
 
 Then /^I see a Disk Failure Message$/ do
-  @screen.wait('GnomeDiskFailureMessage.png',10)
+  @screen.wait('GnomeDiskFailureMessage.png', 10)
 end
