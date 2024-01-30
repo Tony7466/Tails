Tony7466/Tails

commit d723561eb66a8b8a2f1bcb77e3fbdfb63da4be2e

Make check more generic
As Ruff spotted, the call to subprocess.run hasn't check=True,
so it's up to us to deal with all failure modes manually,
so let's ensure we re-raise something for all non-zero exit codes.

config/chroot_local-includes/usr/lib/python3/dist-packages/additional_software.py
@@ -169,8 +169,7 @@ def notify(
         )
         if completed_process.stderr:
             logging.warning("%s", completed_process.stderr)
-        if completed_process.returncode == 1:
-            # sudo failed to execute the command
+        if completed_process.returncode != 0:
             raise OSError(completed_process.stderr)
     except OSError as e:
         logging.warning("Warning: unable to notify the user. %s" % e)

commit a73ebb0fd99498487e52afad4993617f69073259

Fix inter-process communication based on non-zero exit codes
tails-additional-software-notify uses some specific exit codes to tell its
caller what action button the user clicked, so the notify function must
not consider these exit codes are errors.

Fixes regression introduced by d723561eb66a8b8a2f1bcb77e3fbdfb63da4be2e.

config/chroot_local-includes/usr/lib/python3/dist-packages/additional_software.py
@@ -152,6 +152,7 @@ def notify(
         urgent = ""
 
     try:
+        success_exit_codes = (0, 3, 5)
         completed_process = subprocess.run(  # noqa: PLW1510
             [  # noqa: S603
                 "/usr/local/lib/run-with-user-env",
@@ -168,7 +169,7 @@ def notify(
         )
         if completed_process.stderr:
             logging.warning("%s", completed_process.stderr)
-        if completed_process.returncode != 0:
+        if completed_process.returncode not in success_exit_codes:
             raise OSError(completed_process.stderr)
     except OSError as e:
         logging.warning("Warning: unable to notify the user. %s", e)